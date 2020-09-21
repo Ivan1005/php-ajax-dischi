1a83fd97b0ef04118f01fcbb59c6ffc4e36783b7
Ivan <matteraivam@gmail.com>
2020-09-21 12:15:54 +0200

    fatto

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// let $ = require('jquery');
+
+$(document).ready(function() {
+$.ajax(
+  {
+    'url': 'http://localhost/repo-php-ajax-dischi/server.php',
+    'method': "GET",
+    'success': function (database) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+      for (var i = 0; i < database.length; i++) {
+        var context= {
+          "title" : database[i].title,
+          "author" : database[i].author,
+          "year" : database[i].year,
+          "poster" : database[i].poster,
+        };
+        var html = template(context);
+        $(".yes").append(html);
+      }
+    },
+    'error': function () {
+      alert("errore");
+    }
+  }
+ );
+})
